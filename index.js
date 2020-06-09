1e28a7c36f6fb6d9ba54318d54fd5a9841b1ae7f
abdel <a.akroh@e-durable.ch>
2020-06-09 09:16:13 +0200

    method put

--- index.js
+++ index.js
@@ -77,6 +77,48 @@ app.post(
     });
   },
 );
+/************************ */
+
+app.put('/api/users/:id', userValidationMiddlewares, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  const userId = req.params.id;
+  const formData = req.body;
+  return connection.query('UPDATE user SET ? WHERE id = ?', [formData, idUser], (err, results) => {
+    if (err) {
+      if (err.code === 'ER_DUP_ENTRY') {
+        return res.status(409).json({
+          error: 'Email already exists',
+        });
+      }
+      return res.status(500).json({
+        error: err.message,
+        sql: err.sql,
+        body: req.body
+      });
+    }
+
+    return connection.query('SELECT * FROM user WHERE id = ?', userId, (err2, records) => {
+      if (err2) {
+        return res.status(500).json({
+          error: err2.message,
+          sql: err2.sql,
+        });
+      }
+
+      const updatedUser = records[0];
+      const { password, ...user } = updatedUser;
+
+      return res.status(200).json(user);
+    })
+
+  })
+}
+);
+
+
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
